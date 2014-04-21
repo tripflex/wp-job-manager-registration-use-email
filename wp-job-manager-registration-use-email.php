02034278c296bca587081f857156706c97f775d0
Myles McNamara <myles@smyl.es>
2014-04-20 22:13:16 -0400

    rename to match wordpress svn

--- wp-job-manager-registration-use-email.php
+++ wp-job-manager-registration-use-email.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Plugin Name: WP Job Manager - Register Use Email
- * Plugin URI:  https://github.com/tripflex/wp-job-manager-reg-use-email
+ * Plugin Name: WP Job Manager - Registration Use Email
+ * Plugin URI:  https://github.com/tripflex/wp-job-manager-registration-use-email
  * Description: Use email address as username when a new user registers
  * Author:      Myles McNamara
  * Author URI:  http://smyl.es
  * Version:     1.0
- * Text Domain: job_manager_reg_use_email
- * GitHub Plugin URI: tripflex/wp-job-manager-reg-use-email
+ * Text Domain: job_manager_registration_use_email
+ * GitHub Plugin URI: tripflex/wp-job-manager-registration-use-email
  * GitHub Branch:   master
  * @Last Modified by:   Myles McNamara
- * @Last Modified time: 2014-04-19 16:52:09
+ * @Last Modified time: 2014-04-20 22:10:24
  */
 
 // Exit if accessed directly
@@ -18,11 +18,11 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 
 // Set the version of this plugin
-if( ! defined( 'JOB_MANAGER_REGISTER_USE_EMAIL' ) ) {
-	define( 'JOB_MANAGER_REGISTER_USE_EMAIL', '1.0' );
+if( ! defined( 'JOB_MANAGER_REGISTRATION_USE_EMAIL' ) ) {
+	define( 'JOB_MANAGER_REGISTRATION_USE_EMAIL', '1.0' );
 } // end if
 
-class WP_Job_Manager_Register_Use_Email {
+class WP_Job_Manager_Registration_Use_Email {
 	private static $instance;
 
 	public static function instance() {
@@ -40,8 +40,8 @@ class WP_Job_Manager_Register_Use_Email {
 	}
 
 	public static function plugin_activate() {
-		if( JOB_MANAGER_REGISTER_USE_EMAIL != get_option( 'job_manager_register_use_email' ) ) {
-			add_option('job_manager_register_use_email', JOB_MANAGER_REGISTER_USE_EMAIL );
+		if( JOB_MANAGER_REGISTRATION_USE_EMAIL != get_option( 'Job_Manager_Registration_Use_Email' ) ) {
+			add_option('Job_Manager_Registration_Use_Email', JOB_MANAGER_REGISTRATION_USE_EMAIL );
 			$html = '<div class="updated">';
 			$html .= '<p>';
 			$html .= __( '<b>Hooray!</b> Using email as username is ready to go, but you have to enable it <a href="edit.php?post_type=job_listing&page=job-manager-settings#settings-job_submission">on this page</a> under "Job Submission".', 'job_manager_reg_use_email' );
@@ -53,7 +53,7 @@ class WP_Job_Manager_Register_Use_Email {
 	}
 
 	public static function plugin_deactivate(){
-		delete_option( 'job_manager_register_use_email' );
+		delete_option( 'Job_Manager_Registration_Use_Email' );
 	}
 
 	public function job_manager_settings( $settings ) {
@@ -93,7 +93,7 @@ function job_manager_enable_registration_use_email() {
 	return apply_filters( 'job_manager_enable_registration_use_email', get_option( 'job_manager_enable_registration_use_email' ) == 1 ? true : false );
 }
 
-//register_activation_hook( __FILE__, array( 'WP_Job_Manager_Register_Use_Email', 'plugin_activate' ) );
-register_deactivation_hook( __FILE__, array( 'WP_Job_Manager_Register_Use_Email', 'plugin_deactivate' ) );
+//register_activation_hook( __FILE__, array( 'WP_Job_Manager_Registration_Use_Email', 'plugin_activate' ) );
+register_deactivation_hook( __FILE__, array( 'WP_Job_Manager_Registration_Use_Email', 'plugin_deactivate' ) );
 
-add_action( 'init', array( 'WP_Job_Manager_Register_Use_Email', 'instance' ) );
+add_action( 'init', array( 'WP_Job_Manager_Registration_Use_Email', 'instance' ) );
