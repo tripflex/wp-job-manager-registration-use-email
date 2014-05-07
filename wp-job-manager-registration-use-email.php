7564810a30afd4bebdd987d305a84d5225fdb769
Myles McNamara <myles@smyl.es>
2014-05-06 23:35:40 -0400

    functional version

--- wp-job-manager-registration-use-email.php
+++ wp-job-manager-registration-use-email.php
@@ -15,21 +15,38 @@
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
-
+if ( ! defined( 'ABSPATH' ) )
+	exit;
 
 // Set the version of this plugin
-if( ! defined( 'JOB_MANAGER_REGISTRATION_USE_EMAIL' ) ) {
+if ( ! defined( 'JOB_MANAGER_REGISTRATION_USE_EMAIL' ) ) {
 	define( 'JOB_MANAGER_REGISTRATION_USE_EMAIL', '1.2.0' );
 } // end if
 
 class WP_Job_Manager_Registration_Use_Email {
-	private static $instance;
-
 	/**
 	 * @var      string
 	 */
 	public static $plugin_slug = 'wp-job-manager-registration-use-email';
+	private static $instance;
+
+	public function __construct() {
+		add_action( 'admin_notices', array( $this, 'plugin_activate' ) );
+		add_filter( 'job_manager_settings', array(
+				$this,
+				'job_manager_settings'
+			) );
+		add_filter( 'job_manager_create_account_data', array(
+				$this,
+				'job_manager_change_username'
+			) );
+		add_filter( 'gettext', array( $this, 'change_username_label' ) );
+		add_filter( 'plugin_row_meta', array(
+				$this,
+				'add_plugin_row_meta'
+			), 10, 4 );
+
+	}
 
 	public static function instance() {
 		if ( ! isset ( self::$instance ) ) {
@@ -39,17 +56,9 @@ class WP_Job_Manager_Registration_Use_Email {
 		return self::$instance;
 	}
 
-	public function __construct() {
-		add_action( 'admin_notices', array( $this, 'plugin_activate' ) ) ;
-		add_filter( 'job_manager_settings', array( $this, 'job_manager_settings' ) );
-		add_filter( 'job_manager_create_account_data', array( $this, 'job_manager_change_username' ) );
-		add_filter( 'gettext', array( $this, 'change_username_label' ));
-		add_filter( 'plugin_row_meta', array( $this, 'add_plugin_row_meta'), 10, 4 );
-	}
-
 	public static function plugin_activate() {
-		if( JOB_MANAGER_REGISTRATION_USE_EMAIL != get_option( 'Job_Manager_Registration_Use_Email' ) ) {
-			update_option('Job_Manager_Registration_Use_Email', JOB_MANAGER_REGISTRATION_USE_EMAIL );
+		if ( JOB_MANAGER_REGISTRATION_USE_EMAIL != get_option( 'Job_Manager_Registration_Use_Email' ) ) {
+			update_option( 'Job_Manager_Registration_Use_Email', JOB_MANAGER_REGISTRATION_USE_EMAIL );
 			$html = '<div class="updated">';
 			$html .= '<p>';
 			$html .= __( '<b>Hooray!</b> Using email as username is ready to go, but you have to enable it <a href="edit.php?post_type=job_listing&page=job-manager-settings#settings-job_submission">on this page</a> under "Job Submission".', self::$plugin_slug );
@@ -60,32 +69,38 @@ class WP_Job_Manager_Registration_Use_Email {
 		}
 	}
 
-	public static function plugin_deactivate(){
+	public static function plugin_deactivate() {
 		delete_option( 'Job_Manager_Registration_Use_Email' );
 	}
 
-	public function change_username_label($text){
-		if(job_manager_enable_registration_use_email()) {
+	public function change_username_label( $text ) {
+		if ( job_manager_enable_registration_use_email() ) {
+			$custom_label = get_option('job_manager_registration_use_email_custom_username_label');
+			if(!$custom_label) $custom_label = "Username or Email";
 			switch ( $text ) {
 				case 'Username' :
-					$text = __( 'Username or Email', self::$plugin_slug );
+					$text = __( $custom_label, self::$plugin_slug );
 					break;
 			}
 		}
+
 		return $text;
 	}
 
 	public function add_plugin_row_meta( $plugin_meta, $plugin_file, $plugin_data, $status ) {
 		if ( self::$plugin_slug . '/' . self::$plugin_slug . '.php' == $plugin_file ) {
-			$plugin_meta[] = sprintf( '<a href="%s">%s</a>', __( 'http://github.com/tripflex/' . self::$plugin_slug, self::$plugin_slug), __( 'GitHub', self::$plugin_slug ) );
-			$plugin_meta[] = sprintf( '<a href="%s">%s</a>', __( 'https://www.transifex.com/projects/p/' . self::$plugin_slug . '/resource/' . self::$plugin_slug  . '/', self::$plugin_slug ), __( 'Translate', self::$plugin_slug ) );
+			$plugin_meta[] = sprintf( '<a href="%s">%s</a>', __( 'http://github.com/tripflex/' . self::$plugin_slug, self::$plugin_slug ), __( 'GitHub', self::$plugin_slug ) );
+			$plugin_meta[] = sprintf( '<a href="%s">%s</a>', __( 'https://www.transifex.com/projects/p/' . self::$plugin_slug . '/resource/' . self::$plugin_slug . '/', self::$plugin_slug ), __( 'Translate', self::$plugin_slug ) );
 		}
+
 		return $plugin_meta;
 	}
 
 	public function job_manager_settings( $settings ) {
 
-		$use_email = array(
+		wp_enqueue_script( 'job_manager_registration_use_email', plugins_url( '/wpjm-use-email.min.js', __FILE__ ), array( 'jquery' ) );
+
+		$use_email             = array(
 			'name'       => 'job_manager_enable_registration_use_email',
 			'std'        => '0',
 			'label'      => __( 'Registration Username', self::$plugin_slug ),
@@ -100,30 +115,35 @@ class WP_Job_Manager_Registration_Use_Email {
 			'label'      => __( 'Login Username Label', self::$plugin_slug ),
 			'desc'       => __( 'By default when Use Email As Username is enabled, it will change every instance of <code>Username</code> to <code>Username or Email</code>, if you want to use a custom label, enter it here.', self::$plugin_slug ),
 			'type'       => 'input',
-			'attributes' => array()
-		);
+			'attributes' => array(
+				'class'     => 'job_manager_registration_use_email_custom_username_label'
+			)
+	);
 
 		// Get all settings before index 1 in array
-		$settings_before = array_slice($settings['job_submission'][1], 0, 1);
+		$settings_before = array_slice( $settings['job_submission'][1], 0, 1 );
+
 		// Get all settings after index 1 in array
-		$settings_after = array_slice($settings['job_submission'][1], 1);
+		$settings_after = array_slice( $settings['job_submission'][1], 1 );
+
 		// Add new setting option to enable using email
-		$settings_before[] = $use_email;
-		// Add new setting option to use custom username label
-		$settings_before[] = $custom_username_label;
+		array_push( $settings_before, $use_email );
+
+		array_push( $settings_before, $custom_username_label );
+
 		// Merge everything back together
-		$settings['job_submission'][1] = array_merge($settings_before, $settings_after);
+		$settings['job_submission'][1] = array_merge( $settings_before, $settings_after );
 
 		return $settings;
 	}
 
-	public function job_manager_change_username ( $fields ) {
-		if(job_manager_enable_registration_use_email()){
+	public function job_manager_change_username( $fields ) {
+		if ( job_manager_enable_registration_use_email() ) {
 //			Store username in tmp variable so we can change it and still have the original value
-			$username_tmp = $fields['user_login'];
-			$fields['user_login'] = $fields['user_email'];
+			$username_tmp           = $fields['user_login'];
+			$fields['user_login']   = $fields['user_email'];
 			$fields['display_name'] = $username_tmp;
-			$fields['nickname'] = $username_tmp;
+			$fields['nickname']     = $username_tmp;
 		}
 
 		return $fields;
@@ -136,6 +156,12 @@ function job_manager_enable_registration_use_email() {
 }
 
 //register_activation_hook( __FILE__, array( 'WP_Job_Manager_Registration_Use_Email', 'plugin_activate' ) );
-register_deactivation_hook( __FILE__, array( 'WP_Job_Manager_Registration_Use_Email', 'plugin_deactivate' ) );
+register_deactivation_hook( __FILE__, array(
+		'WP_Job_Manager_Registration_Use_Email',
+		'plugin_deactivate'
+	) );
 
-add_action( 'init', array( 'WP_Job_Manager_Registration_Use_Email', 'instance' ) );
+add_action( 'init', array(
+		'WP_Job_Manager_Registration_Use_Email',
+		'instance'
+	) );
